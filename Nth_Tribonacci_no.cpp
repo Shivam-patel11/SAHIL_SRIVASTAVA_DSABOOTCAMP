438a6b60f940eb35171c8d36b906f0f851017fdc
Shivam-patel11 <77850452+Shivam-patel11@users.noreply.github.com>
2021-11-17 00:27:14 +0530

    Update Nth_Tribonacci_no.cpp

--- Nth_Tribonacci_no.cpp
+++ Nth_Tribonacci_no.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int tribonacci(int n) {
         unordered_map<int,int>mp;
-        return ans(n,mp);
+        return tribonacciNo(n,mp);
     }
-    int ans(int n,unordered_map<int,int>&mp){
+    int tribonacciNo(int n,unordered_map<int,int>&mp){
         if(n <= 1){
             return n;
         }
@@ -15,9 +15,9 @@ public:
         if(mp.find(currKey)!=mp.end()){
             return mp[currKey];
         }
-        int a=ans(n-1,mp);
-        int b=ans(n-2,mp);
-        int c=ans(n-3,mp);
+        int a=tribonacciNo(n-1,mp);
+        int b=tribonacciNo(n-2,mp);
+        int c=tribonacciNo(n-3,mp);
          mp[currKey]=a+b+c;
         return a+b+c;
     }
